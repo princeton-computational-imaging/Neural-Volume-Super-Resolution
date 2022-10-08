-DTU: Small changes to assure evaluating over the same scenes even when training on a subset. -Small bug fixes -Enabling plane normalization prior to SR -Enabling plane consistency loss for SR -For spatial sampling, enabling sampling by patch STD instead of non-(estimated)background content -Adding virtual batch capability

load_DTU.py
@@ -91,13 +91,6 @@ class DVRDataset(torch.utils.data.Dataset):
             cat = os.path.basename(base_dir)
             with open(file_list, "r") as f:
                 objs = [(cat, os.path.join(base_dir, x.strip())) for x in f.readlines()]
-            # if max_scenes is not None and len(objs)+len(self.all_objs)>max_scenes:
-            #     # For saving time during debug
-            #     print('!!! Keeping only %d scenes!!!'%(max_scenes))
-            #     if len(self.all_objs)>=max_scenes:
-            #         if len(self.val_scenes)==0: self.val_scenes = [max_scenes-1]
-            #         break
-            #     objs = objs[:max_scenes-len(self.all_objs)]
             if l_num in val_lists:  self.val_scenes.extend([i+len(self.all_objs) for i in range(len(objs))])
             self.all_objs.extend(objs)
 
@@ -105,9 +98,8 @@ class DVRDataset(torch.utils.data.Dataset):
 
         if max_scenes is not None and len(self.all_objs)>max_scenes:    # For saving time during debug
             print('!!! Keeping only %d scenes!!!'%(max_scenes))
-            self.all_objs,self.val_scenes = subsample_dataset(scenes_dict=self.all_objs,max_scenes=max_scenes,val_only_scenes=self.val_scenes,scene_id_func=self.DTU_sceneID)
-            # scenes_list = [self.sceneObj_2_name(obj) for obj in self.all_objs]
-            # self.val_scenes = [i for i,s in enumerate(self.val_scene_IDs()) if s in scenes_list]
+            self.all_objs,self.val_scenes = subsample_dataset(scenes_dict=self.all_objs,max_scenes=max_scenes,
+                val_only_scenes=self.val_scenes,scene_id_func=self.DTU_sceneID,max_val_only_scenes=None)
 
         if excluded_scenes is not None:
             scene_names = [self.DTU_sceneID(i) for i in range(len(self.all_objs))]

models.py
@@ -678,19 +678,21 @@ class PlanesOptimizer(nn.Module):
         self.scenes = list(scene_id_plane_resolution.keys())
         if training_scenes is None:
             training_scenes = 1*self.scenes
-        assert all([s in self.scenes for s in training_scenes])
         self.training_scenes = training_scenes
-        self.scene_sampler = SceneSampler(self.training_scenes,do_when_reshuffling=do_when_reshuffling)
-        self.models = {}
-        self.buffer_size = options.buffer_size
-        assert use_coarse_planes,'Unsupported yet, probably requires adding a param_group to the optimizer'
-        assert not init_params or optimize,'This would means using (frozen) random planes...' 
-        self.use_coarse_planes = use_coarse_planes
-        self.save_location = save_location
-        assert optimizer_type=='Adam'
-        self.lr = lr
+        self.buffer_size = getattr(options,'buffer_size',len(self.training_scenes))
         self.steps_per_buffer,self.steps_since_drawing = options.steps_per_buffer,0
         if self.buffer_size>=len(self.training_scenes):  self.steps_per_buffer = -1
+        assert all([s in self.scenes for s in self.training_scenes])
+        assert optimizer_type=='Adam','Optimizer %s not supported yet.'%(optimizer_type)
+        assert use_coarse_planes,'Unsupported yet, probably requires adding a param_group to the optimizer'
+        assert not init_params or optimize,'This would means using (frozen) random planes...'
+        assert self.steps_per_buffer==-1 or self.steps_per_buffer>=self.buffer_size,\
+            'Trying to use %d steps for a buffer of size %d: Some scenes would be loaded in vain.'%(options.steps_per_buffer,self.buffer_size)
+        self.scene_sampler = SceneSampler(self.training_scenes,do_when_reshuffling=do_when_reshuffling)
+        self.models = {}
+        self.use_coarse_planes = use_coarse_planes
+        self.save_location = save_location
+        self.lr = lr
         for model_name,model in zip(['coarse','fine'],[model_coarse,model_fine]):
             self.models[model_name] = model
             if model_name=='fine' and use_coarse_planes:    continue
@@ -735,6 +737,17 @@ class PlanesOptimizer(nn.Module):
 
     def param_path(self,model_name,scene):
         return os.path.join(self.save_location,"%s_%s.par"%(model_name,scene))
+
+    def get_plane_stats(self,viewdir=False):
+        model_name='coarse'
+        plane_means,plane_STDs = [],[]
+        for scene in tqdm(self.training_scenes,desc='Collecting plane statistics'):
+            loaded_params = torch.load(self.param_path(model_name=model_name,scene=scene))
+            for k,p in loaded_params['params'].items():
+                if not viewdir and get_plane_name(None,self.models[model_name].N_PLANES_DENSITY) in k:  continue
+                plane_means.append(torch.mean(p,(2,3)).squeeze(0))
+                plane_STDs.append(torch.std(p.reshape(p.shape[1],-1),1))
+        return {'mean':torch.stack(plane_means,0).mean(0),'std':torch.stack(plane_STDs,0).mean(0)}
 
     def save_params(self,to_checkpoint=False):
         assert self.optimize,'Why would you want to save if not optimizing?'
@@ -807,8 +820,8 @@ class PlanesOptimizer(nn.Module):
                 self.optimizer.state = defaultdict(dict,[(params[i],v) for i,v in enumerate(optimizer_states) if v is not None])
         self.saving_needed = False
 
-    def step(self):
-        if self.optimize:
+    def step(self,opt_step=True):
+        if self.optimize and opt_step:
             self.optimizer.step()
             self.saving_needed = True
         self.steps_since_drawing += 1
@@ -843,7 +856,8 @@ class _Residual_Block(nn.Module):
         return output 
 
 class EDSR(nn.Module):
-    def __init__(self,scale_factor,in_channels,out_channels,hidden_size,plane_interp,n_blocks=32):
+    def __init__(self,scale_factor,in_channels,out_channels,hidden_size,plane_interp,n_blocks=32,
+        input_normalization=False,consistentcy_loss_w:float=None):
         super(EDSR, self).__init__()
 
         # rgb_mean = (0.4488, 0.4371, 0.4040)
@@ -890,7 +904,12 @@ class EDSR(nn.Module):
                     m.bias.data.zero_()
 
         self.clear_SR_planes(all_planes=True)
-        # self.SR_updated = {},
+        if input_normalization:
+            self.normalization_params({'mean':float('nan')*torch.ones([in_channels]),'std':float('nan')*torch.ones([in_channels])})
+        self.consistentcy_loss_w = consistentcy_loss_w
+        if consistentcy_loss_w is not None:
+            self.planes_diff = nn.L1Loss()
+            self.consistentcy_loss = []
 
     def make_layer(self, block,args, num_of_layer):
         layers = []
@@ -900,6 +919,10 @@ class EDSR(nn.Module):
 
     def interpolate_LR(self,id):
         return torch.nn.functional.interpolate(self.LR_planes[id].cuda(),scale_factor=self.scale_factor,mode=self.plane_interp,align_corners=self.align_corners)
+
+    def normalization_params(self,norm_dict):
+        self.planes_mean_NON_LEARNED = nn.Parameter(norm_dict['mean'].reshape([1,-1,1,1]))
+        self.planes_std_NON_LEARNED = nn.Parameter(norm_dict['std'].reshape([1,-1,1,1]))
 
     def residual_plane(self,id):
         if id in self.residual_planes:
@@ -922,6 +945,13 @@ class EDSR(nn.Module):
         for attr in planes_2_clear:
             setattr(self,attr,{})
 
+    def return_consistency_loss(self):
+        loss = None
+        if self.consistentcy_loss_w is not None and len(self.consistentcy_loss)>0:
+            loss = torch.mean(torch.stack(self.consistentcy_loss))
+            self.consistentcy_loss = []
+        return loss
+
     def forward(self, plane_name):
         if isinstance(plane_name,tuple):
             full_plane = False
@@ -935,36 +965,45 @@ class EDSR(nn.Module):
             # assert plane_roi is None,'Unsupported yet'
             out = self.SR_planes[plane_name].cuda()
         else:
-            x = self.LR_planes[plane_name].detach().cuda()
-            plane_roi = (torch.tensor(x.shape[2:])).to(plane_roi.device)*(1+plane_roi)/2
+            LR_plane = self.LR_planes[plane_name].detach().cuda()
+            if hasattr(self,'planes_mean_NON_LEARNED'):
+                LR_plane = LR_plane-self.planes_mean_NON_LEARNED
+                LR_plane = LR_plane/self.planes_std_NON_LEARNED
+            plane_roi = (torch.tensor(LR_plane.shape[2:])).to(plane_roi.device)*(1+plane_roi)/2
             plane_roi = torch.stack([torch.floor(plane_roi[0]),torch.ceil(plane_roi[1])],0).cpu().numpy().astype(np.int32)
             plane_roi[0] = np.maximum(0,plane_roi[0]-1)
-            plane_roi[1] = np.minimum(np.array(x.shape[2:]),plane_roi[1]+1)
+            plane_roi[1] = np.minimum(np.array(LR_plane.shape[2:]),plane_roi[1]+1)
             pre_padding = np.minimum(plane_roi[0],self.required_padding)
-            post_padding = np.minimum(np.array(x.shape[2:])-plane_roi[1],self.required_padding)
+            post_padding = np.minimum(np.array(LR_plane.shape[2:])-plane_roi[1],self.required_padding)
             take_last = lambda ind: -ind if ind>0 else None
             DEBUG = False
             if DEBUG:   print('!! WARNING !!!!')
-            x = torch.nn.functional.pad(x[...,plane_roi[0,0]-pre_padding[0]:plane_roi[1,0]+post_padding[0],
-                plane_roi[0,1]-pre_padding[1]:plane_roi[1,1]+post_padding[1]],
+            x = LR_plane[...,plane_roi[0,0]-pre_padding[0]:plane_roi[1,0]+post_padding[0],plane_roi[0,1]-pre_padding[1]:plane_roi[1,1]+post_padding[1]]
+            x = torch.nn.functional.pad(x,
                 pad=(self.required_padding-pre_padding[1],self.required_padding-post_padding[1],self.required_padding-pre_padding[0],self.required_padding-post_padding[0]),
                 mode='replicate') 
             difference = self.inner_forward(x)[...,self.HR_overpadding:take_last(self.HR_overpadding),self.HR_overpadding:take_last(self.HR_overpadding)]
+            # if '_D0' in plane_name:
+            #     print('!!!!!!WARNING!!!!!!!!')
+            #     import matplotlib.pyplot as plt
+            #     plt.plot(torch.std(difference.reshape([difference.shape[1],-1]),1).cpu().numpy(),'+')
             min_index = plane_roi[0]*self.scale_factor
             max_index = plane_roi[1]*self.scale_factor
-            if DEBUG:
-                residual_plane = self.residual_plane(plane_name)[...,min_index[0]:max_index[0],min_index[1]:max_index[1]]
-            else:
-                residual_plane = self.residual_plane(plane_name)
+            residual_plane = self.residual_plane(plane_name)
+            super_resolved = torch.add(difference,residual_plane[...,min_index[0]:max_index[0],min_index[1]:max_index[1]])
             out = (torch.ones_like(residual_plane)*float('nan'))
-            if DEBUG:
-                out = torch.add(difference,residual_plane)
-            else:
-                out[...,min_index[0]:max_index[0],min_index[1]:max_index[1]] = torch.add(
-                    difference,
-                    residual_plane[...,min_index[0]:max_index[0],min_index[1]:max_index[1]])
-
-                if full_plane:   self.SR_planes[plane_name] = out.cpu()
+            out[...,min_index[0]:max_index[0],min_index[1]:max_index[1]] = super_resolved
+            if full_plane:   self.SR_planes[plane_name] = out.cpu()
+            if self.consistentcy_loss_w is not None and self.training:
+                self.consistentcy_loss.append(
+                    self.planes_diff(
+                        LR_plane[...,plane_roi[0,0]:plane_roi[1,0],plane_roi[0,1]:plane_roi[1,1]],
+                        torch.nn.functional.interpolate(
+                            super_resolved,scale_factor=1/self.scale_factor,mode=self.plane_interp,
+                            align_corners=self.align_corners,antialias=True
+                            )
+                    )
+                )
         return out
 
     def inner_forward(self,x):

nerf_helpers.py
@@ -41,6 +41,14 @@ def set_config_defaults(source,target):
     for k in source.keys():
         if k not in target: setattr(target,k,getattr(source,k))
 
+def image_STD_2_distribution(patch_size):
+    unfold = torch.nn.Unfold(kernel_size=(patch_size,patch_size))
+    def mapper(image):
+        assert image.dim()==3 and image.shape[2]==3
+        im_STD = unfold(image.permute(2,0,1).unsqueeze(0)).reshape(1,3,patch_size**2,-1).std(2).reshape(3,image.shape[0]-patch_size+1,image.shape[1]-patch_size+1).mean(0)
+        return im_STD/im_STD.sum()
+    return mapper
+
 def is_background_white(image):
     perimeter = torch.cat([image[0,...].reshape([-1]),image[-1,...].reshape([-1]),image[:,0,...].reshape([-1]),image[:,-1,...].reshape([-1]),])
     black_portion = (perimeter<=10/256).float().mean()
@@ -58,7 +66,7 @@ def is_background_white(image):
     # else:
     #     return 0
 
-def subsample_dataset(scenes_dict,max_scenes,val_only_scenes=[],scene_id_func=None):
+def subsample_dataset(scenes_dict,max_scenes,val_only_scenes=[],scene_id_func=None,max_val_only_scenes=None):
     convert2list, = False,
     val_scene_as_inds = len(val_only_scenes)>0 and isinstance(val_only_scenes[0],int)
     if val_scene_as_inds:
@@ -74,7 +82,10 @@ def subsample_dataset(scenes_dict,max_scenes,val_only_scenes=[],scene_id_func=No
     sampled_dict = dict([i_val_others[i] for i in np.unique(np.round(np.linspace(0,len(i_val_others)-1,max_scenes)).astype(int))])
     sampled_val_only_scenes = []
     if len(i_val_val_only)>0:
-        sampled_val_only_scenes = [i_val_val_only[i] for i in np.unique(np.round(np.linspace(0,len(i_val_val_only)-1,max_scenes)).astype(int))]
+        if max_val_only_scenes is None:
+            sampled_val_only_scenes = i_val_val_only
+        else:
+            sampled_val_only_scenes = [i_val_val_only[i] for i in np.unique(np.round(np.linspace(0,len(i_val_val_only)-1,max_val_only_scenes)).astype(int))]
         sampled_dict.update(dict(sampled_val_only_scenes))
         sampled_val_only_scenes = [s[0] for s in sampled_val_only_scenes]
         if val_scene_as_inds:
@@ -84,11 +95,13 @@ def subsample_dataset(scenes_dict,max_scenes,val_only_scenes=[],scene_id_func=No
     return sampled_dict,sampled_val_only_scenes
 
 
-def compute_patches_distribution(image,patch_size):
-    white_bg = is_background_white(image)
-    image = integral_image(torch.any(image<246/256 if white_bg else image>10/256,-1))
-    image = image[patch_size:,patch_size:]+image[:-(patch_size),:-(patch_size)]-image[patch_size:,:-(patch_size)]-image[:-(patch_size),patch_size:]
-    return image.float()/image.sum()
+def estimated_background_2_distribution(patch_size):
+    def mapper(image):
+        white_bg = is_background_white(image)
+        image = integral_image(torch.any(image<246/256 if white_bg else image>10/256,-1))
+        image = image[patch_size:,patch_size:]+image[:-(patch_size),:-(patch_size)]-image[patch_size:,:-(patch_size)]-image[:-(patch_size),patch_size:]
+        return image.float()/image.sum()
+    return mapper
 
 def integral_image(image):
     return torch.cat([torch.zeros([1+image.shape[0],1]).type(image.type()),torch.cat([torch.zeros([1,image.shape[1]]).type(image.type()),torch.cumsum(torch.cumsum(image,0),1)],0)],1)

train_nerf.py
@@ -129,7 +129,6 @@ def main():
         if dataset_type=='synt':
             train_dirs = assert_list(cfg.dataset.dir.train)
             val_only_dirs = assert_list(getattr(cfg.dataset.dir,'val',[]))
-            # basedirs = [os.path.join(cfg.dataset.root,d) for d in train_dirs+val_only_dirs]
             basedirs = train_dirs+val_only_dirs
             images, poses, render_poses, hwfDs, i_split,scene_ids = [],torch.zeros([0,4,4]),[],[[],[],[],[]],[np.array([]).astype(np.int64) for i in range(3)],[]
             scene_id,scene_id_plane_resolution,val_only_scene_ids,coords_normalization = -1,{},[],{}
@@ -138,7 +137,6 @@ def main():
                 scene_id_plane_resolution = {0:max(plane_resolutions),1:min(plane_resolutions)}
             for basedir in tqdm(basedirs,desc='Loading scenes'):
                 for ds_num,factor in enumerate(downsampling_factor):
-                    # scene_id += 1
                     scene_id = ''+basedir
                     val_only = basedir not in train_dirs
                     if val_only:    val_only_scene_ids.append(scene_id)
@@ -157,9 +155,7 @@ def main():
                         coords_normalization[basedir] =\
                             calc_scene_box({'camera_poses':cur_poses.numpy()[:,:3,:4],'near':cfg.dataset.near,'far':cfg.dataset.far,'H':cur_hwfDs[0],'W':cur_hwfDs[1],'f':cur_hwfDs[2]},including_dirs=cfg.nerf.use_viewdirs)
                     i_val[scene_id] = [v+len(images) for v in cur_i_split[1]]
-                    i_train[scene_id] = [v+len(images) for v in cur_i_split[0]]
-                    # for i in range(len(i_split)):
-                    #     i_split[i] = np.concatenate((i_split[i],cur_i_split[i]+len(images)))
+                    if not val_only:    i_train[scene_id] = [v+len(images) for v in cur_i_split[0]]
                     images += cur_images
                     poses = torch.cat((poses,cur_poses),0)
                     for i in range(len(hwfDs)):
@@ -181,7 +177,6 @@ def main():
             scene_ids = dataset.scene_IDs()
             total_scenes_num = dataset.num_scenes() #+dataset_eval.num_scenes()
             assert len(plane_resolutions)==1
-
             scene_id_plane_resolution = dict(zip([dataset.DTU_sceneID(i) for i in range(total_scenes_num)],[plane_resolutions[0] for i in range(total_scenes_num)]))
             basedirs,coords_normalization = [],{}
             scene_iterator = range(dataset.num_scenes()) if load_saved_models else trange(dataset.num_scenes(),desc='Computing scene bounding boxes')
@@ -192,16 +187,9 @@ def main():
                     coords_normalization[dataset.DTU_sceneID(id)] = calc_scene_box(scene_info,including_dirs=cfg.nerf.use_viewdirs)
                 basedirs.append(dataset.DTU_sceneID(id))
             i_val = dataset.i_val
-            # assert all([len(i_val[basedirs[0]])==len(i_val[id]) for id in basedirs]),'Assuming all scenes have the same number of evaluation images'
             i_train = dataset.train_ims_per_scene
-        # assert all([len(i_val[basedirs[0]])==len(i_val[id]) for id in basedirs]),'Assuming all scenes have the same number of evaluation images'
         if hasattr(cfg.dataset,'max_scenes_eval'):
-            i_val,val_only_scene_ids = subsample_dataset(scenes_dict=i_val,max_scenes=cfg.dataset.max_scenes_eval,val_only_scenes=val_only_scene_ids)
-            # i_val_val_only = [(k,v) for k,v in i_val.items() if k in val_only_scene_ids]
-            # i_val_others = [(k,v) for k,v in i_val.items() if k not in val_only_scene_ids]
-            # i_val = dict([i_val_others[i] for i in np.unique(np.round(np.linspace(0,len(i_val_others)-1,cfg.dataset.max_scenes_eval)).astype(int))])
-            # if len(i_val_val_only)>0:
-            #     i_val.update(dict([i_val_val_only[i] for i in np.unique(np.round(np.linspace(0,len(i_val_val_only)-1,cfg.dataset.max_scenes_eval)).astype(int))]))
+            i_val,val_only_scene_ids = subsample_dataset(scenes_dict=i_val,max_scenes=cfg.dataset.max_scenes_eval,val_only_scenes=val_only_scene_ids,max_val_only_scenes=cfg.dataset.max_scenes_eval)
         scenes4which2save_ims = 1*list(i_val.keys())
         if hasattr(cfg.dataset,'max_scene_savings'):
             raise Exception('Should be fixed after adding max_scenes_eval')
@@ -256,7 +244,6 @@ def main():
     else:
         encode_position_fn = None
         encode_direction_fn = None
-    # if getattr(cfg.models.coarse,"use_viewdirs",False) or getattr(cfg.models.fine,"use_viewdirs",False):    assert cfg.nerf.use_viewdirs
     if planes_model:
         if hasattr(cfg.nerf.train,'viewdir_downsampling'):  assert hasattr(cfg.nerf.train,'max_plane_downsampling')
         store_planes = hasattr(cfg.nerf.train,'store_planes')
@@ -348,6 +335,8 @@ def main():
                 hidden_size=cfg.super_resolution.model.hidden_size,
                 plane_interp=model_fine.plane_interp,
                 n_blocks=cfg.super_resolution.model.n_blocks,
+                input_normalization=cfg.super_resolution.get("input_normalization",False),
+                consistentcy_loss_w=cfg.super_resolution.get("consistentcy_loss_w",0),
             )
             print("SR model: %d parameters"%(num_parameters(SR_model)))
         else:
@@ -385,12 +374,13 @@ def main():
                 num_layers_dir=cfg.super_resolution.model.get("num_layers_dir",1),
                 hidden_size=cfg.super_resolution.model.hidden_size,
                 dirs_hidden_width_ratio=1,
-                xyz_input_2_dir=cfg.super_resolution.model.get("xyz_input_2_dir",False)
+                xyz_input_2_dir=cfg.super_resolution.model.get("xyz_input_2_dir",False),
             )
             print("SR model: %d parameters, input dimension xyz: %d, dirs: %d"%\
                 (num_parameters(SR_model),SR_input_dim[0],SR_input_dim[1]))
         SR_model.to(device)
-        trainable_parameters = list(SR_model.parameters())
+        # trainable_parameters = list(SR_model.parameters())
+        trainable_parameters = [p for k,p in SR_model.named_parameters() if 'NON_LEARNED' not in k]
     else:
         # Initialize optimizer.
         def collect_params(model,filter='all'):
@@ -464,8 +454,6 @@ def main():
         if not ok:  raise Exception('Inconsistent model config')
         checkpoint = torch.load(checkpoint)
         def update_saved_names(state_dict):
-            # if any(['planes_.sc0' in k for k in state_dict.keys()]):
-            #     assert len([k for k in state_dict if 'planes_.sc' in k])==len([k for k in model_coarse.state_dict() if 'planes_.sc' in k])
             return state_dict
             if any(['planes_.' in k and '.sc' not in k for k in state_dict.keys()]):
                 return OrderedDict([(k.replace('planes_.','planes_.sc0_res32_D'),v) for k,v in state_dict.items()])
@@ -507,9 +495,20 @@ def main():
                 model_coarse.load_state_dict(checkpoint["model_coarse_state_dict"])
                 model_fine.load_state_dict(checkpoint["model_fine_state_dict"])
         del checkpoint # Importent: Releases GPU memory occupied by loaded data.
-    # # TODO: Prepare raybatch tensor if batching random rays
+
     spatial_padding_size = SR_model.receptive_field//2 if isinstance(SR_model,models.Conv3D) else 0
     spatial_sampling = spatial_padding_size>0 or cfg.nerf.train.get("spatial_sampling",False)
+    if spatial_padding_size>0 or spatial_sampling:
+        SAMPLE_PATCH_BY_CONTENT = True
+        patch_size = chunksize_to_2D(cfg.nerf.train.num_random_rays)
+        patch_size = int(np.ceil(spatial_sampling*patch_size))
+        if SAMPLE_PATCH_BY_CONTENT:
+            assert getattr(cfg.nerf.train,'spatial_patch_sampling','background_est') in ['background_est','STD']
+            if getattr(cfg.nerf.train,'spatial_patch_sampling','background_est')=='STD':
+                im_2_sampling_dist = image_STD_2_distribution(patch_size=patch_size)
+            else:
+                im_2_sampling_dist = estimated_background_2_distribution(patch_size=patch_size)
+
     assert isinstance(spatial_sampling,bool) or spatial_sampling>=1
     if planes_model and SR_model is not None:
         save_RAM_memory = not store_planes and len(model_coarse.planes_)>=10
@@ -558,6 +557,9 @@ def main():
             do_when_reshuffling=lambda:scenes_cycle_counter.step(print_str='Number of scene cycles performed: '),
             STD_factor=getattr(cfg.nerf.train,'STD_factor',0.1),
         )
+    if SR_experiment=="model" and getattr(cfg.super_resolution,'input_normalization',False) and not os.path.exists(configargs.load_checkpoint):
+        #Initializing a new SR model that uses input normalization
+        SR_model.normalization_params(planes_opt.get_plane_stats(viewdir=getattr(cfg.super_resolution,'SR_viewdir',False)))
 
     downsampling_offset = lambda ds_factor: (ds_factor-1)/(2*ds_factor)
     saved_target_ims = dict(zip(val_strings,[set() for i in val_strings]))#set()
@@ -565,6 +567,7 @@ def main():
         spatial_margin = SR_model.receptive_field//2 if spatial_sampling else None
     else:
         spatial_margin = 0
+    virtual_batch_size = getattr(cfg.nerf.train,'virtual_batch_size',1)
 
     def evaluate():
         tqdm.write("[VAL] =======> Iter: " + str(iter))
@@ -702,6 +705,8 @@ def main():
         return loss,psnr
 
     def train():
+        first_v_batch_iter = iter%virtual_batch_size==0
+        last_v_batch_iter = iter%virtual_batch_size==(virtual_batch_size-1)
         if SR_experiment=="model":
             SR_model.train()
         else:
@@ -739,11 +744,8 @@ def main():
             dim=-1,
         )
         if spatial_padding_size>0 or spatial_sampling:
-            SAMPLE_PATCH_BY_CONTENT = True
-            patch_size = chunksize_to_2D(cfg.nerf.train.num_random_rays)
-            patch_size = int(np.ceil(spatial_sampling*patch_size))
             if SAMPLE_PATCH_BY_CONTENT:
-                patches_vacancy_dist = compute_patches_distribution(img_target[...,:3],patch_size)
+                patches_vacancy_dist = im_2_sampling_dist(img_target[...,:3])
                 upper_left_corner = torch.argwhere(torch.rand([])<torch.cumsum(patches_vacancy_dist.reshape([-1]),0))[0].item()
                 upper_left_corner = np.array([upper_left_corner//patches_vacancy_dist.shape[1],upper_left_corner%patches_vacancy_dist.shape[1]])
             else:
@@ -773,8 +775,9 @@ def main():
         ray_directions = ray_directions[select_inds[:, 0], select_inds[:, 1], :]
         batch_rays = torch.stack([ray_origins, ray_directions], dim=0)
 
-        optimizer.zero_grad()
-        if store_planes:    planes_opt.zero_grad()
+        if first_v_batch_iter:
+            optimizer.zero_grad()
+            if store_planes:    planes_opt.zero_grad()
         rgb_coarse, _, _, rgb_fine, _, _,rgb_SR,_,_ = run_one_iter_of_nerf(
             cur_H if not spatial_sampling else patch_size,
             cur_W if not spatial_sampling else patch_size,
@@ -796,6 +799,7 @@ def main():
             loss = torch.nn.functional.mse_loss(
                     rgb_SR[..., :3], target_ray_values[..., :3]
                 )
+
         else:
             coarse_loss = torch.nn.functional.mse_loss(
                 rgb_coarse[..., :3], target_ray_values[..., :3]
@@ -810,16 +814,23 @@ def main():
                 elif getattr(cfg.super_resolution.training,'loss','both')=='coarse': fine_loss = None
             # loss = torch.nn.functional.mse_loss(rgb_pred[..., :3], target_s[..., :3])
             loss = (coarse_loss if coarse_loss is not None else 0.0) + (fine_loss if fine_loss is not None else 0.0)
+
+        writer.add_scalar("train/loss", loss.item(), iter)
+        if SR_experiment=="model" and planes_model:
+            SR_consistency_loss = SR_model.return_consistency_loss()
+            if SR_consistency_loss is not None:
+                writer.add_scalar("train/inconsistency", SR_consistency_loss.item(), iter)
+                loss += cfg.super_resolution.consistentcy_loss_w*SR_consistency_loss
+
         loss.backward()
         psnr = mse2psnr(loss.item())
         new_drawn_scenes = None
         if store_planes:
-            new_drawn_scenes = planes_opt.step()
-
-        optimizer.step()
-        # If training an SR model operating on planes, discarding super-resolved planes after updating the model:
-        if planes_model and SR_experiment=='model': SR_model.clear_SR_planes()
-        writer.add_scalar("train/loss", loss.item(), iter)
+            new_drawn_scenes = planes_opt.step(opt_step=last_v_batch_iter)
+        if last_v_batch_iter:
+            optimizer.step()
+            # If training an SR model operating on planes, discarding super-resolved planes after updating the model:
+            if planes_model and SR_experiment=='model': SR_model.clear_SR_planes()
         if SR_experiment!="model" or planes_model:
             if coarse_loss is not None:
                 writer.add_scalar("train/coarse_loss", coarse_loss.item(), iter)
@@ -842,8 +853,8 @@ def main():
         else:
             evaluate_now = iter % cfg.experiment.validate_every == 0
         evaluate_now |= iter == cfg.experiment.train_iters - 1
-        # print('!!!!!!!!!!WARNING')
-        # evaluate_now = False
+        # print('!!!!!!!!!!WARNING!!!!!!!!!!!')
+        # evaluate_now = True
         if evaluate_now:
             last_evaluated = 1*iter
             start_time = time.time()
@@ -877,7 +888,7 @@ def main():
                 + str(np.mean(print_cycle_psnr))
             )
             print_cycle_loss,print_cycle_psnr = [],[]
-        save_now = scenes_cycle_counter.check_and_reset() if store_planes else False
+        save_now = scenes_cycle_counter.check_and_reset() if (store_planes and not SR_experiment) else False
         save_now |= iter % cfg.experiment.save_every == 0 if isinstance(cfg.experiment.save_every,int) else (time.time()-recently_saved)/60>cfg.experiment.save_every
         save_now |= iter == cfg.experiment.train_iters - 1
         save_now &= iter>0

train_utils.py
@@ -326,8 +326,9 @@ def run_one_iter_of_nerf(
 
     # chunk_size = getattr(options.nerf, mode).chunksize*64//max([getattr(options.nerf, mode).num_coarse,getattr(options.nerf, mode).num_fine])
     chunk_size = getattr(options.nerf, mode).chunksize
+    # print('!!!!!!WARNING!!!!!!!!')
     if 'della-' in socket.gethostname():
-        chunk_size *= 5
+        chunk_size *= 4
     if (SR_model is not None or hasattr(model_fine,'SR_model')):
         chunk_size //= 10 #5 #(2*int(np.ceil(np.log2(model_fine.SR_model.n_blocks))))
         # if model_fine.SR_model.training: